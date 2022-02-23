f1323cad555854ca11eeec88ad8fd61f16e059ac
Daniela Alves <danizinhaalves.360@gmail.com>
2022-02-22 21:50:58 -0300

    testenada

--- src/br/com/fundatec/locadoraveiculo/LocadoraVeiculo.java
+++ src/br/com/fundatec/locadoraveiculo/LocadoraVeiculo.java
@@ -11,7 +11,6 @@ public class LocadoraVeiculo {
     public static void main(String[] args) {
         MenuPrincipal menuPrincipal = new MenuPrincipal();
         menuPrincipal.imprimirMenu();
-
     }
 }
 
